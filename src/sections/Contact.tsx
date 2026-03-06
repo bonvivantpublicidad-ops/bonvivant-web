dcb369088f39ae7ddfca8934007bb73b1bcf37c0
bonvivantpublicidad-ops <bonvivantpublicidad@gmail.com>
2026-03-06 13:11:45 -0300

    Update Contact.tsx

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -138,11 +138,17 @@ const Contact = () => {
                   <Instagram className="w-5 h-5 text-bonvivant-gold group-hover:text-bonvivant-cream transition-colors" />
                 </a>
                 <a
-                  href="#"
+                  href="https://www.instagram.com/bonvivantpublicidad"
+                      target="_blank"
+                      rel="noopener noreferrer"
                   className="w-12 h-12 rounded-full bg-bonvivant-black flex items-center justify-center hover:bg-bonvivant-red transition-colors group"
                 >
                   <Facebook className="w-5 h-5 text-bonvivant-gold group-hover:text-bonvivant-cream transition-colors" />
                 </a>
+                <a
+                  href="https://www.facebook.com/Bonvivant.grafica"
+                      target="_blank"
+                      rel="noopener noreferrer"
               </div>
             </div>
           </div>
